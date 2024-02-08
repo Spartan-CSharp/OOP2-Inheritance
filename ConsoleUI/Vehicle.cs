e33843154759d024e673e0d19c9577f3580866c2
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 12:09:47 -0500

    Create Code
    Create a Vehicle Class, a Car Class, a Boat Class, and a Motorcycle Class. Identify what inheritance should happen, if any, and wire it up.

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+	internal class Vehicle
+	{
+		internal int NumberOfSeats
+		{
+			get; set;
+		}
+		internal string Engine
+		{
+			get; set;
+		}
+
+		internal void StartEngine()
+		{
+		}
+
+		internal void StopEngine()
+		{
+		}
+	}
+}
